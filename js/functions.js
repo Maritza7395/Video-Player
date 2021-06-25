94e6f35d608f9f32e81fd5a05197398666f527bf
Maritza Loaiza Urrutia <mloaiza.urrutia@gmail.com>
2021-06-24 22:38:11 -0400

    finish

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,6 @@
 const video = document.getElementById('video');
 const progress = document.getElementById("progress");
 const timer = document.getElementById("timer");
-let pausa;
 
 function start() {
     queries();
@@ -13,8 +12,17 @@ function queries() {
     $('#row-play').css('display', 'none');
     $('#row-btns').css('display', 'block');
     $('.progreso').css('display', 'block');
-    $("#player").mouseenter(function() { $('#row-btns').css('display', 'block'); $('.progreso').css('display', 'block'); });
-    $("#player").mouseleave(function() { $('#row-btns').css('display', 'none'); $('.progreso').css('display', 'none'); });
+    $('.title').css('display', 'block');
+    $("#player").mouseenter(function() { 
+        $('#row-btns').css('display', 'block'); 
+        $('.progreso').css('display', 'block'); 
+        $('.title').css('display', 'block');
+    });
+    $("#player").mouseleave(function() { 
+        $('#row-btns').css('display', 'none'); 
+        $('.progreso').css('display', 'none'); 
+        $('.title').css('display', 'none');
+    });
 }
 
 function queryPause() {
@@ -22,6 +30,7 @@ function queryPause() {
     $('#row-play').css('display', 'block');
     $('#row-btns').css('display', 'none');
     $('.progreso').css('display', 'none');
+    $('.title').css('display', 'none');
 }
 
 function pause() {
@@ -39,8 +48,14 @@ function progressLoop() {
         console.log(progress.value);
         let duracion = Math.round(video.currentTime); 
         timer.innerHTML = secondsToString(duracion); 
-        if(progress.value >= 6 && progress.value <= 91){
-            timer.style.marginLeft = (progress.value - 3) + '%';
+        if(window.innerWidth <= 768){
+            if(progress.value >= 6 && progress.value <= 81){
+                timer.style.marginLeft = (progress.value - 3) + '%';
+            }
+        } else{
+            if(progress.value >= 6 && progress.value <= 91){
+                timer.style.marginLeft = (progress.value - 3) + '%';
+            }
         }
     }, 1000);
 }
